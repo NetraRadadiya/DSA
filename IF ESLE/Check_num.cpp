e325e68bd1c9e5af3aeecc8d38e19b377f29093b
Radadiya Netra <148869437+NetraRadadiya@users.noreply.github.com>
2025-02-04 09:04:30 +0530

    Add files via upload

---
+++ IF ESLE/Check_num.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+	
+	int num;
+	
+	cout <<"Enetr your Number : " ;
+	cin >> num;
+	
+	if(num > 0)
+	{
+		cout <<	"The Number is Positive";  
+	}
+	else if(num < 0)
+	{
+		cout << "The Number Negative" ; 
+	}
+	else{
+		cout << "The Number Is Nautral"; 
+	}
+	
+}
+
---
+++ IF ESLE/NUMBER_DIVIDE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	int num;
+	
+	cout <<" Enter Your number : ";
+	cin >> num;
+	
+	if(num % 7 ==0)
+	{
+		cout <<"The Number is Divisible"; 
+	}
+	else
+	{
+		cout <<"The Number is Not Divisible";
+	}
+}
