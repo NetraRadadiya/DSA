5711b3a7ab25998cea2764bd0198e6ddac08d2f5
Radadiya Netra <148869437+NetraRadadiya@users.noreply.github.com>
2025-02-06 21:18:37 +0530

    4max.cpp

--- Nested If Else/4max.cpp
+++ Nested If Else/4max.cpp
@@ -1,90 +1,57 @@
-//#include<iostream>
-//using namespace std;
-//
-//int main(){
-//	
-//	int a,b,c,d;
-//	cout << "Enter A :";
-//	cin >> a;
-//	cout << "Enter B :";
-//	cin >> b;
-//	cout << "Enter C :";
-//	cin >> c;
-//	cout << "Enter D :";
-//	cin >> d;
-//	
-//	if(a>b){
-//		if(a>c){
-//			if(a>d){
-//				cout << "A is Max";
-//			}
-//			else{
-//				cout << "D is Max";
-//			}
-//		}
-//		else{
-//			cout << "C is Max";
-//		}
-//	}
-//	else{
-//		if(b>c){
-//			if(b>d){
-//				cout << "B is Max";
-//			}
-//			else{
-//				cout << "D is Max";
-//			}
-//		}
-//		else{
-//			cout << "C is Max";
-//		}
-//	}
-//	
-//	return 0;
-//}
-
-
 #include<iostream>
 using namespace std;
 
 int main(){
-    
-    int a, b, c, d;
-    cout << "Enter A :";
-    cin >> a;
-    cout << "Enter B :";
-    cin >> b;
-    cout << "Enter C :";
-    cin >> c;
-    cout << "Enter D :";
-    cin >> d;
-    
-    if(a > b){
-        if(a > c){
-            if(a > d){
-                cout << "A is Max";
-            }
-            else{
-                cout << "D is Max";
-            }
-        }
-        else{
-            cout << "C is Max";
-        }
-    }
-    else{
-        if(b > c){
-            if(b > d){
-                cout << "B is Max";
-            }
-            else{
-                cout << "D is Max";
-            }
-        }
-        else{
-            cout << "C is Max";
-        }
-    }
-    
-    return 0;
+	
+	int a,b,c,d;
+	cout << "Enter A :";
+	cin >> a;
+	cout << "Enter B :";
+	cin >> b;
+	cout << "Enter C :";
+	cin >> c;
+	cout << "Enter D :";
+	cin >> d;
+	
+	if(a>b){
+		if(a>c){
+			if(a>d){
+				cout << "A is Max";
+			}
+			else{
+				cout << "D is Max";
+			}
+		}
+		else{
+			if(c>d){
+				cout << "C is Max";
+			}
+			else
+			{
+				cout << "D is max";
+			}
+		}
+	}
+	else{
+		if(b>c){
+			if(b>d){
+				cout << "B is Max";
+			}
+			else{
+				cout << "D is Max";
+			}
+		}
+		else{
+			if(c>d)
+			{
+				cout << "C is max";
+			}
+			else
+			{
+				cout << "D is max";
+			}
+		}
+	}
+	
+	return 0;
 }
