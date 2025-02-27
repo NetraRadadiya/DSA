f3cbf44c59d2ea65029f05a9538199bf6dc1bed1
Radadiya Netra <148869437+NetraRadadiya@users.noreply.github.com>
2025-02-27 22:29:03 +0530

    Add files via upload

---
+++ Patten/Custum_Patten/b.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for(int i=1; i<=7; i++)
+	{
+		for(int j=1; j<=5; j++)
+		{
+			if(j==1)
+			{
+				cout << "* ";
+			}
+			else if((i==1 || i==7 || i==4) && j<=3)
+			{
+				cout << "* ";	
+			}
+			else if((i==2 || i==6) && j==4)
+			{
+				cout << "* ";	
+			}
+				else if((i==3 || i==5) && j==4)
+			{
+				cout << "* ";	
+			}
+			else
+			{
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
