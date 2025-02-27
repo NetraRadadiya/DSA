e78406eaf56225105293de3c52e1b3fcd30a6a7b
Radadiya Netra <148869437+NetraRadadiya@users.noreply.github.com>
2025-02-27 22:27:19 +0530

    Add files via upload

---
+++ Patten/Space_patten/p14.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{	
+	for(char i='A' ;i<='E'; i++)
+	{
+		for(char k=i; k<='D'; k++)
+		{
+			cout << "  ";
+		}
+		for(char j='A'; j<=i; j++)
+		{
+			cout << j << " ";
+		}
+		cout << endl;
+	}
+	
+	
+}
---
+++ Patten/Space_patten/p16.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{	
+	for(char i='A'; i<='E'; i++)
+	{
+		for(char j=i; j<='E'; j++)
+		{
+			cout << j << " ";
+		}
+		
+		cout << endl;
+		
+		for(char k='A'; k<=i; k++)
+		{
+			cout << "  ";
+		}
+	}
+	
+	
+}
